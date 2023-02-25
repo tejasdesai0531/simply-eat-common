acfbef6a3c033d7bcf41dcf9f07e6c7edc451cb5
Tejas Desai <tejadesai0531@gmail.com>
2023-02-25 19:55:10 +0530

    Updates

--- index.js
+++
@@ -1,6 +0,0 @@
-exports.Listner = require('./src/events/base-listener')
-exports.Publisher = require('./src/events/base-publisher')
-
-
-exports.BadRequestError = require('./src/errors/bad-request-error')
-exports.RequestValidationError = require('./src/errors/request-validation-error')
---
+++ src/index.js
@@ -0,0 +1,6 @@
+module.exports.Listner = require('./events/base-listener')
+module.exports.Publisher = require('./events/base-publisher')
+
+
+module.exports.BadRequestError = require('./errors/bad-request-error')
+module.exports.RequestValidationError = require('./errors/request-validation-error')
