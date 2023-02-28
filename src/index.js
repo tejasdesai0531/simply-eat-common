0ab9c230907162aee88f5311ffb62196de7ce564
Tejas Desai <tejadesai0531@gmail.com>
2023-02-28 22:55:51 +0530

    Updates

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const Listner = require('./events/base-listener')
 const Publisher = require('./events/base-publisher')
 
+const CustomError = require('./errors/custom-error')
 const BadRequestError = require('./errors/bad-request-error')
 const RequestValidationError = require('./errors/request-validation-error')
+const NotFoundError = require('./errors/not-found-error')
 
 module.exports = {
     Listner,
     Publisher,
     
+    CustomError,
     BadRequestError,
-    RequestValidationError
+    RequestValidationError,
+    NotFoundError
 }
